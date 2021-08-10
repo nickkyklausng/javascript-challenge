12472e7eb1a8773da209f78bfc55ba395f380148
Sophia Brill <sophia@Sophias-Air.modem>
2021-08-10 21:24:22 +1000

    final changes

--- **UFO-level-1**/static/js/app.js
+++ **UFO-level-1**/static/js/app.js
@@ -1,46 +1,44 @@
 // from data.js
 var tableData = data;
+var tbody =  d3.select("tbody")
+//YOUR CODE HERE!
+function table(tableData) {
+tableData.forEach(function(datas) {
+    var row = tbody.append("tr");
+    Object.entries(datas).forEach(function([key, value]) {
 
-// YOUR CODE HERE!
-var tbody = d3.select("tbody");
-var inputDate = d3.select("#datetime");
-var inputCity = d3.select("#city");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
-
-
-// Input data into the HTML
-
-var inputData = (dataInput) => {
-    dataInput.forEach(ufoSightings => {
-        var row = tbody.append("tr");
-        columns.forEach(column => row.append("td").text(ufoSightings[column])
-        )
+        var cell = row.append("td");
+        cell.text(value);
     });
-}
-//reference data
-inputData(tableData);
+});
+};
 
+//print table data
+table(tableData);
 
-// Create an Event Listener for the Button
-
- // Select the button
 var button = d3.select("#filter-btn");
+button.on("click",runEnter)
 
-// Getting a reference to the input element on the page with the id property.
-var inputField = d3.select("#datetime");
-
-// This function is triggered when the button is clicked
-function handleClick() {
-    console.log("A button was clicked!");
-    //test using d3 to see if object dispatched the event
-    console.log(d3.event.target);
-}
-// Input fields can trigger a change event when new text is entered.
-inputField.on("change", function() {
-    var newText = d3.event.target.value;
-    console.log(newText);
-  });
-  
+// create a function that clears original table
+function clearTable() {
+    // select the body and remvoe all tr and td elements
+    d3.select("tbody")
+      .selectAll("tr").remove()
+      .selectAll("td").remove();
+  };
 
 
-// Setup Filter Button for Date and City
+function runEnter() {
+    d3.event.preventDefault();
+    clearTable();
+
+    //select input element and user input
+    var InputElement = d3.select("#datetime");
+    var InputValue = InputElement.property("value");
+
+    var filteredData = tableData.filter(data => data.datetime === InputValue.trim());
+    // console check for correct data being filtered
+    console.log(filteredData)
+    // insert filtered data into table
+    table(filteredData)
+};
